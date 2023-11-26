04311f0b7d27b90eaa8288bf05591f6cff6b29c5
Andres Yepes <andresarad@misena.edu.co>
2023-11-26 12:35:15 -0500

    Add: fixture

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents() {},
     defaultCommandTimeout: 20000,
     pageLoadTimeout: 20000,
   },
--- cypress/pages/fillformHealth.ts
+++ cypress/pages/fillformHealth.ts
@@ -4,50 +4,44 @@ const helthCare = Cypress.env("healthcare");
 
 export class FillFormHealth {
 
-    goToHealthCare(){
+    goToHealthCare() {
         cy.visit(helthCare.url)
-        }
-    goLoginPage(){
+    }
+
+    goLoginPage() {
         cy.get(loginPage.makeAppointment).click();
     }
 
-    fillLogin(){
-        cy.get(loginPage.demoUserName).invoke('val').then((texto)=> {
+    fillLogin() {
+        cy.get(loginPage.demoUserName).invoke('val').then((texto) => {
             if (texto !== undefined) {
                 cy.get(loginPage.username).type(texto.toString());
             }
         })
-        cy.get(loginPage.demoPassword).invoke('val').then((texto)=> {
-            if (texto !== undefined){
+        cy.get(loginPage.demoPassword).invoke('val').then((texto) => {
+            if (texto !== undefined) {
                 cy.get(loginPage.password).type(texto.toString());
             }
-        })               
-        
+        })
         cy.get(loginPage.btnLogin).click();
-
     }
 
-    fillMakeAppointment(){
-        const aleatorio = Math.floor(Math.random()*3)
+    fillMakeAppointment() {
+        const aleatorio = Math.floor(Math.random() * 3)
         cy.get(formPage.facility).select(aleatorio);
-
         cy.get(formPage.checkboxReadmision).check();
-        
         cy.get(formPage.checkRadio).then((radios) => {
             cy.wrap(radios[aleatorio]).click();
         })
-    
-        cy.get(formPage.visitDate).type("29/11/2023");
-
-        cy.get(formPage.commentPlace).type("En la cita necesito que me revisen un dolor en la columna y otro en la nariz", {force:true});
-       
+        cy.fixture("healthForm.json").then((userdata) => {
+            cy.get(formPage.visitDate).type(userdata.fecha);
+            cy.get(formPage.commentPlace).type(userdata.comment, { force: true });
+        })
         cy.get(formPage.btnBook).click();
-
     }
 
-    validateAppointment(){
+    validateAppointment() {
         cy.get(confirmationPage.msgConfirmation).should('contain', 'Please be informed that your appointment has been booked as following:');
         cy.get(confirmationPage.h2).should('contain', 'Appointment Confirmation');
-
     }
 }
